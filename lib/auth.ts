2cb77ea29b1a7991be0c3cc43047f1cdbc76f61d
StableMusicSPAI <stablemusicspai@gmail.com>
2025-11-27 02:15:30 +0100

    Delete lib directory

--- lib/auth.ts
+++
@@ -1,6 +0,0 @@
-import { getServerSession } from "next-auth";
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export async function getSession(req: NextApiRequest, res: NextApiResponse) {
-  return await getServerSession(req, res, {});
-}
